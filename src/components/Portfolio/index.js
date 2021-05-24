7fbd8cb5e8368a2a74539c2b8c9dea252a234774
Curtis Smith <cj1988333@hotmail.com>
2021-05-23 23:21:32 -0400

    added portfolio card

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import projects from '../../utils/portfolio.json';
+import PortfolioItem from '../PortfolioItem';
+
 
 function Portfolio(){
     return(
-        <section>test</section>
+        <section className="about section-padding" id="about">
+            <div className="container">
+                <h1 className="my-5 text-center"><i className="far fa-file-code mr-5" />My Portfolio</h1>
+                <div className="row">
+                    {<PortfolioItem projects={projects} />}
+                </div>
+            </div>
+        </section>
     );
    
 }
 export default Portfolio;
+
+
+
---
+++ src/components/PortfolioItem/index.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import image from "../../assets/images/portfolio/Thumbnail/DogDash.png";
+
+function PortfolioItem({projects}){
+    const[onHover, setOnHover] = useState(false);
+console.log(projects);
+    return(
+        projects.map(project =>(
+            <div className="card text-white col-md-4 my-5" key={project.id} onMouseEnter={()=>setOnHover(project.id)} onMouseLeave={()=>setOnHover(false)}>
+                    <img 
+                        src={require(`../../assets/images/portfolio/Thumbnail/${project.image}`).default} 
+                        className="card-img img-fluid img-thumbnail" 
+                    />
+                    {onHover===project.id && (
+                        <div className="card-img-overlay opac text-center text-dark p-5" >
+                            <h3 className="text-dark mt-3"><b>{project.title}</b></h3>
+                            <div className="row">
+                                {/* render only if links are available: */}
+                                {project.link && (<div className={project.github?"col-6":"col-12" }><a href={project.link} className="fas fa-link largeicon" target="_blank"/><div>Live Site</div></div>)}
+                                {project.github && (<div className={project.link?"col-6":"col-12" }><a href={project.github} className="fab fa-github largeicon" target="_blank"/><div>Git Repo</div></div>)}
+
+                            </div>
+                            <p className="custom-btn btn card-title text-center mx-3">Focus: <b>{project.category}</b></p>
+                        </div>
+                    )}
+            </div>
+        ))
+    )
+}
+export default PortfolioItem;
